6baeec1cf494b5a6ce68741ed792c0653bbc087b
lukeWaninger <luke.waninger@gmail.com>
2019-01-24 19:06:27 -0800

    added test dir

---
+++ tests/test_load.py
@@ -0,0 +1,28 @@
+from unittest import skip, TestCase
+
+
+class TestLoad(TestCase):
+    """test class for loading features into S3
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        """perform at test class initialization
+        """
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        """perform when all tests are complete
+        """
+        pass
+
+    def setUp(self):
+        """perform before each unittest"""
+        pass
+
+    def tearDown(self):
+        """perform after each unittest
+        """
+        pass
+

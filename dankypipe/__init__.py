cff9c5f517b08883dca2b4b47b0c7b0b737d506e
lukeWaninger <luke.waninger@gmail.com>
2019-01-31 12:13:14 -0800

    .

--- dankypipe/__init__.py
+++ dankypipe/__init__.py
@@ -1,7 +1,11 @@
-name = 'danky-pipe'
+name = 'dankypipe'
 
 __all__ = [
     'constants',
     'pipe',
     'runner'
 ]
+
+import dankypipe.constants
+import dankypipe.runner
+import dankypipe.pipe
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="danky-pipe",
+    name="dankypipe",
     version="0.0.1",
     author="Vivek Pagadala, Luke Waninger",
     author_email="luke.waninger@gmail.com",

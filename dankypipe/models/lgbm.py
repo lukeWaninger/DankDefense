a23b4b3414ef678b9c0e0b22d6116236a46b4bf6
lukeWaninger <luke.waninger@gmail.com>
2019-02-03 19:46:03 -0800

    .

--- dankypipe/models/lgbm.py
+++ dankypipe/models/lgbm.py
@@ -9,10 +9,6 @@ class Model:
 
     def train(self, x, y):
         lgb_train = lgb.Dataset(x, y)
-
-        with open('a_small_demo_log.txt', 'w') as f:
-            f.write(self.kwargs)
-
         self.model = lgb.train(self.parameters['params'], lgb_train, **self.kwargs)
 
     def predict(self, x):

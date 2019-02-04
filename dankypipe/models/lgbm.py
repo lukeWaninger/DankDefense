b701e51f06f48460a9817915665d26867f02b54b
lukeWaninger <luke.waninger@gmail.com>
2019-02-03 16:59:10 -0800

    .

--- dankypipe/models/lgbm.py
+++ dankypipe/models/lgbm.py
@@ -7,11 +7,14 @@ class Model:
         self.model = None
 
         if 'kwargs' not in self.parameters.keys():
-            self.parameters['kwargs'] = {}
+            self.kwargs = {}
+        else:
+            self.kwargs = self.parameters['kwargs']
+            del self.parameters['kwargs']
 
     def train(self, x, y):
         lgb_train = lgb.Dataset(x, y)
-        self.model = lgb.train(self.parameters['params'], lgb_train, **self.parameters['kwargs'])
+        self.model = lgb.train(self.parameters['params'], lgb_train, **self.kwargs)
 
     def predict(self, x):
         return self.model.predict(x)
